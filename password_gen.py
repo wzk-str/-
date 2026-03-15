04324b99f86b64d78b56569ae82968c0475640ea
wzk <2103556904@qq.com>
2026-03-15 10:17:27 +0800

    sdlf

---
+++ password_gen.py
@@ -0,0 +1,10 @@
+import random, string, sys
+
+def generate_password(length=12):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(chars) for _ in range(length))
+    return password
+
+if __name__ == '__main__':
+    length = int(sys.argv[1]) if len(sys.argv) > 1 else 12
+    print(generate_password(length))
